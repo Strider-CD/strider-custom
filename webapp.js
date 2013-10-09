a040293905eacc897238fea2d92802d347a153df
Jared Forsyth <jared@jaredforsyth.com>
2013-10-09 14:50:17 -0600

    using codemirror for script input

--- webapp.js
+++ webapp.js
@@ -1,6 +1,12 @@
 
 module.exports = {
   config: {
-    custom: {}
+    custom: {
+      environment: {type: String, default: '# type shell commands here'},
+      prepare: {type: String, default: '# type shell commands here'},
+      test: {type: String, default: '# type shell commands here'},
+      deploy: {type: String, default: '# type shell commands here'},
+      cleanup: {type: String, default: '# type shell commands here'}
+    }
   }
 }
