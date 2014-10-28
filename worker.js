1ca383ac21b74e1533d040ba7276c1bbe9fca666
Ilya Radchenko <ilya@burstcreations.com>
2014-10-28 13:45:59 -0400

    Strip out comments when building shell command

--- worker.js
+++ worker.js
@@ -1,19 +1,32 @@
-function shellCommand(command) {
-  if (!command || !command.replace(/#[^\n]*/g, '').trim().length) return
-  return {
-    command: 'sh',
-    args: ['-x', '-c', command]
-  }
-}
+'use strict';
+
 module.exports = {
   init: function (config, context, done) {
-    var config = config || {}
+    var config = config || {};
+    
     done(null, {
       environment: shellCommand(config.environment),
       prepare: shellCommand(config.prepare),
       test: shellCommand(config.test),
       deploy: shellCommand(config.deploy),
       cleanup: shellCommand(config.cleanup)
-    })
+    });
   }
+};
+
+function shellCommand(command) {
+  if (!command) {
+    return;
+  }
+  
+  var normalizedCommand = command.replace(/#[^\n]*/g, '').trim();
+  
+  if (!normalizedCommand.length) {
+    return;
+  }
+  
+  return {
+    command: 'sh',
+    args: ['-x', '-c', normalizedCommand]
+  };
 }
