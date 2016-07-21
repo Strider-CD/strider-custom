5d5b88dbb8beb826680cdfdcd8879819e5b868d8
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-07-21 02:40:26 +0200

    Allow custom screen output for commands

--- worker.js
+++ worker.js
@@ -28,10 +28,30 @@ function shellCommand(command, shell, job) {
     return;
   }
 
-  var commands = Array.isArray(command) ? command : command.split('\n');
+  var commands;
+  if (Array.isArray(command)) {
+    // The plugin was passed an array of commands to run.
+    commands = command;
 
-  // Generate individual command descriptions for each given command.
-  if (1 < commands.length) {
+  } else if (typeof command === 'string') {
+    // The plugin was passed a string, to be treated as a single command.
+    commands = command.split('\n');
+
+  } else if (typeof command === 'object') {
+    // The plugin was passed an object, which is expected to have a "command" member, containing the command as a string.
+    // Additionally, the object might contain a "screen" member to control the output in the web UI.
+    if (command.command) {
+      commands = command.command.split('\n');
+      commands.screen = command.screen;
+    }
+  }
+  if (!commands) {
+    debug('Invalid command passed.', command);
+    return null;
+  }
+
+  // Generate individual command descriptions for each given command, if the input was an array.
+  if (1 < commands.length && Array.isArray(command)) {
     return commands.map(function (commandLine) {
       return shellCommand(commandLine, shell, job);
     });
@@ -53,25 +73,29 @@ function shellCommand(command, shell, job) {
   if ((/bash/i).test(shell)) {
     return {
       command: 'bash',
-      args: ['-e', '-x', '-c', commandToExecute]
+      args: ['-e', '-x', '-c', commandToExecute],
+      screen: commands.screen
     };
 
   } else if ((/powershell/i).test(shell)) {
     return {
       command: 'powershell',
-      args: ['-NonInteractive', '-Command', commandToExecute]
+      args: ['-NonInteractive', '-Command', commandToExecute],
+      screen: commands.screen
     };
 
   } else if (process.platform === 'win32') {
     return {
       command: 'cmd',
-      args: ['/c', commandToExecute]
+      args: ['/c', commandToExecute],
+      screen: commands.screen
     };
   }
 
   return {
     command: 'sh',
-    args: ['-e', '-x', '-c', commandToExecute]
+    args: ['-e', '-x', '-c', commandToExecute],
+    screen: commands.screen
   };
 }
 
